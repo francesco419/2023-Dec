295e5bfcdab13e8efcab38b786978ceb560d39b3
Lee Sang Hyeon <francesco419@naver.com>
2024-06-08 23:20:02 +0900

    230508

--- src/page/project/project.tsx
+++ src/page/project/project.tsx
@@ -6,16 +6,25 @@ import {
   PerspectiveCamera
 } from '@react-three/drei';
 import { ModelCoin } from './Bitcoin';
-import { Model } from './ex/Benz';
-import { ModelCoinN } from './BitcoinN';
 import { useState } from 'react';
 
 const AUTOROTATESPEED = 5;
 const ENABLEZOOM = false;
 const POLARANGLE = 1.5;
 
+interface XYZ_Type {
+  x: number;
+  y: number;
+  z: number;
+}
+
 const Project = () => {
   const [foo, setFoo] = useState<boolean>(true);
+  const [xyz, setXyz] = useState<XYZ_Type>({
+    x: 10,
+    y: 10,
+    z: 10
+  });
 
   const pointEnter = () => {
     setFoo((foo) => false);
@@ -25,10 +34,45 @@ const Project = () => {
     setFoo((foo) => true);
   };
 
+  /**useFrame((state, delta) => {
+    easing.damp3(state.camera.position, [state.pointer.x, 1 + state.pointer.y / 2, 8 + Math.atan(state.pointer.x * 2)], 0.3, delta)
+    state.camera.lookAt(state.camera.position.x * 0.9, 0, -4)
+  }) */
+
+  const xyzHandler = (axis: string) => {
+    let temp: XYZ_Type = xyz;
+    if (axis === 'x') {
+      temp.x = temp.x + 10;
+      console.log(temp);
+      setXyz((xyz) => temp);
+      return;
+    }
+    if (axis === 'y') {
+      temp.y = temp.y + 10;
+      console.log(temp);
+      setXyz((xyz) => temp);
+      return;
+    }
+    if (axis === 'z') {
+      temp.z = temp.z + 10;
+      console.log(temp);
+      setXyz((xyz) => temp);
+      return;
+    }
+  };
+
   return (
-    <Canvas>
-      <Environment preset='apartment' />
-      {/* <OrbitControls
+    <>
+      <Canvas
+        camera={{
+          fov: 75,
+          near: 0.1,
+          far: 1000,
+          position: [xyz.x, xyz.y, xyz.z]
+        }}
+      >
+        <Environment preset='apartment' />
+        {/* <OrbitControls
         autoRotate={foo}
         autoRotateSpeed={AUTOROTATESPEED}
         enableDamping
@@ -37,17 +81,22 @@ const Project = () => {
         maxPolarAngle={POLARANGLE}
         minPolarAngle={POLARANGLE}
       /> */}
-      <PerspectiveCamera position={[10, 0, 10]} rotation={[30, 20, 10]} />
-      <ambientLight intensity={10} color={0xffffff} />
-      {/*
+        {/* <ambientLight intensity={10} color={0xffffff} /> */}
+        {/*
         <pointLight color={0xffffff} intensity={10} />
         <directionalLight
           color={0xffffff}
           intensity={10}
           position={[0, 0, 0]}
         /> */}
-      <ModelCoin onPointerEnter={pointEnter} onPointerLeave={pointLeave} />
-    </Canvas>
+        <ModelCoin onPointerEnter={pointEnter} onPointerLeave={pointLeave} />
+      </Canvas>
+      <div style={{ position: 'absolute', top: '100px', left: '100px' }}>
+        <button onClick={() => xyzHandler('x')}>x</button>
+        <button onClick={() => xyzHandler('y')}>y</button>
+        <button onClick={() => xyzHandler('z')}>z</button>
+      </div>
+    </>
   );
 };
 
